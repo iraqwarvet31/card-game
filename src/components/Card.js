b9649e3a00cc8cec69b47e5fcd0593eb6e6fe4de
Larry Olguin <iraqwarvet31@gmail.com>
2020-08-30 12:11:40 -0700

    changed DOM selector for unmatched cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 let selectedCards = [];
 
@@ -8,70 +8,50 @@ class Card extends Component {
     super(props);
     this.state = {
       isFlipped: false,
-      visible: false,
     }
-  }
-
-  componentDidMount() {
-    this.setState({ visible: true })
   }
 
   toggleClass = (event) => {
+
+    selectedCards.push(event.currentTarget.id)
+    console.log(selectedCards)
     
-    if (selectedCards.length > 2) selectedCards = []
+    if (selectedCards.length === 2) {
+      var card1 = document.getElementsByClassName(selectedCards[0])[0]
+      var card2 = document.getElementsByClassName(selectedCards[1])[0]
 
-    selectedCards.push((this.props.index).toString())
-
-    if (selectedCards[0] === selectedCards[1]) {
-      let pair1 = document.getElementsByClassName(selectedCards[0])[0]
-      let pair2 = document.getElementsByClassName(selectedCards[1])[1]
-      
-      console.log('MATCH FOUND!!')
-      console.log(selectedCards)
-      
-      setTimeout(() => pair1.style.visibility = 'hidden', 900)
-      setTimeout(() => pair2.style.visibility = 'hidden', 900)
-
-      selectedCards = []
-    } 
-
-    if (selectedCards[0] !== selectedCards[1] && selectedCards.length === 2) {
-      let pair1 = document.getElementsByClassName(selectedCards[0])[0]
-      let pair2 = document.getElementsByClassName(selectedCards[1])[1]
-      // console.log(selectedCards, pair1,pair2)
-      setTimeout(() => pair1.click(), 900)
-      setTimeout(() => pair2.click(), 900)
-  
+      console.log(this.state.isFlipped)
+      setTimeout(() => {
+        card1.classList.remove('is-flipped')
+        card2.classList.remove('is-flipped')
+      }, 1000)
       selectedCards = []
     }
-      
     this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }))
-}
+  }
 
   render() {
-  
     return (
-      <div className={this.state.visible ? "fade-in scene" : "scene"} >
+      <div className="fade-in scene">
         <div
           className={
-            this.state.isFlipped ? `card is-flipped ${ this.props.index }` : 
-            `card ${ this.props.index }` 
+            this.state.isFlipped
+              ? `card is-flipped ${this.props.id}`
+              : `card ${this.props.id}`
           }
-          onClick={ this.toggleClass }
+          onClick={this.toggleClass}
+          id={this.props.id}
         >
-          <div className="card__face card__face--front">
-          </div>
+          <div
+            className="card__face card__face--front"
+          ></div>
           <div className="card__face card__face--back">
-            <FontAwesomeIcon 
-              icon={ this.props.icon }
-              color="coral" 
-              size="2x"
-            />
+            <FontAwesomeIcon icon={this.props.icon} color="coral" size="2x" />
           </div>
         </div>
       </div>
-    )     
-  } 
+    );
+  }
 }
 
 export default Card;
